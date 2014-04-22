2ba8532c2ed3769b1189c4672b02afac4a8e02f3
Teet Talviste <teet.talviste@gmail.com>
2014-04-22 13:31:10 +0300

    add setup files

---
+++ setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+import sys
+
+sys.path.insert(0, "misc")
+
+from distribute_setup import use_setuptools
+use_setuptools()
+import setuptools
+
+import version
+
+setuptools.setup(
+    name="opentsdb.snmp",
+    version=version.get_git_version(),
+    install_requires=[
+        "netsnmp"
+    ],
+    test_require=[
+        "nose>=1.0",
+        "mock>=0.8."
+    ],
+    entry_points={
+        "console_scripts": [
+            "opentsdb-snmp=opentsdb.snmp.main:run"
+        ],
+        "resolvers": [
+            "default=opentsdb.snmp.resolvers.default:Default",
+            "ifname=opentsdb.snmp.resolvers.ifname:IfName"
+        ]
+
+    }
+
+)
