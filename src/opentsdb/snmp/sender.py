2a8b02a24c1d664274f1043a73b17d7f0c865736
Teet Talviste <teet.talviste@gmail.com>
2014-06-09 12:42:05 +0300

    send data to tsdb in bigger chunks

--- src/opentsdb/snmp/sender.py
+++ src/opentsdb/snmp/sender.py
@@ -177,7 +177,7 @@ class SenderThread(multiprocessing.Process):
                 line = self.squeue.get(True, self.queue_timeout)
                 senddata.append(line)
                 self.squeue.task_done()
-                if len(senddata) > 100:
+                if len(senddata) > 500000:
                     break
             except Exception:
                 #logging.debug("Queue empty")
